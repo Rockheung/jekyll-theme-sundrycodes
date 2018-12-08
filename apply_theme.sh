f53eb249cfb1f0890f4c1d2fcf2a2d2c79a14f42
Rockheung <rockheung@gmail.com>
2018-12-09 01:38:04 +0900

    release

--- apply_theme.sh
+++ apply_theme.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-declare -a arr=("assets" "_layouts" "_includes" "_sass")
+declare -a arr=("assets" "_layouts" "_includes" "_sass" "package.json")
+declare here=$(pwd)
 
 for i in "${arr[@]}"
 do
     cp -Tvr $HOME/git/jekyll-theme-sundrycodes/"$i" $HOME/git/rockheung.github.io/"$i"
 done
 
+cd $HOME/git/rockheung.github.io
+npm install
+bundle exec jekyll serve
+
+cd ${here}
